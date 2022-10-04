ed974a94558b43d53f1e12eccbf0f6164b4bf961
Nick <Nick@DESKTOP-CD5UUTH>
2022-10-04 23:50:30 +0300

    Add project files.

---
+++ ConvertCurrency/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Converter
+{
+    public decimal dollar, euro;
+    public Converter(decimal dollar, decimal euro)
+    {
+        this.dollar = dollar;
+        this.euro = euro;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
